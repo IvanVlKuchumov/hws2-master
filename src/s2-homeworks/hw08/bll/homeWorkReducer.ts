2536bde8da817325ea28c0b85efe33a9dd56e793
IvanVlKuchumov <kuchumoviv@gmail.com>
2023-01-29 20:38:54 +0300

    HW8 solution

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,41 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            const compareFunctionUp = (a:UserType, b:UserType) => {
+                if (a.name > b.name) {
+                    return 1;
+                }
+                if (a.name < b.name) {
+                    return -1;
+                }
+                return 0;
+            }
+            const compareFunctionDown = (a:UserType, b:UserType) => {
+                if (a.name < b.name) {
+                    return 1;
+                }
+                if (a.name > b.name) {
+                    return -1;
+                }
+                return 0;
+            }
+            const compareFunction = action.payload === 'up' ? compareFunctionUp : compareFunctionDown
+            return state.sort(compareFunction) // need to fix
         }
         case 'check': {
-
-            return state // need to fix
+            // const compareFunctionUp = (a:UserType, b:UserType) => {
+            //     if (a.name > b.name) {
+            //         return 1;
+            //     }
+            //     if (a.name < b.name) {
+            //         return -1;
+            //     }
+            //     return 0;
+            // }
+            return state.filter(f => f.age > 17) // need to fix
         }
         default:
             return state
